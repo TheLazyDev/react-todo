9cb97e7d42132e4452be41fa8b405b659943d435
Bhavarsh Singh <bhavarshs@gmail.com>
2017-04-18 18:35:32 +0530

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -6,12 +6,12 @@ try {
 
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyBKKPfMbeOE27O5eri6SSZjDvfXkod85us",
-        authDomain: "mead-todo-app-4a74f.firebaseapp.com",
-        databaseURL: "https://mead-todo-app-4a74f.firebaseio.com",
-        projectId: "mead-todo-app-4a74f",
-        storageBucket: "mead-todo-app-4a74f.appspot.com",
-        messagingSenderId: "283668995048"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,22 @@ var webpack = require('webpack');
 var path = require('path');
 
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var envFile = require('node-env-file');
 
 
+try {
+
+
+    envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+
+} catch (e) {
+
+}
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+console.log(process.env.NODE_ENV);
 
 module.exports = {
     entry: [
@@ -24,6 +37,18 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                API_KEY: JSON.stringify(process.env.API_KEY),
+                AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+                DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+                STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+                PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+                MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+
             }
         })
     ],
